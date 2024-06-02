7fea9733ef2c954df27ae0211ce6472358cf562e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-02 12:02:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const argv = require('yargs').argv;
 const source = argv.source || path.join(__dirname, 'LoginSignUp');
 const destination = argv.destination || path.join(process.cwd(), 'src/components/LoginSignUp');
 
+const sourceTW = argv.source || path.join(__dirname, 'LoginSignUpTW');
+const destinationTW = argv.destination || path.join(process.cwd(), 'src/components/LoginSignUpTW');
+
+
 async function RunReactLoginSignIn() {
     try {
         await fs.copy(source, destination);
