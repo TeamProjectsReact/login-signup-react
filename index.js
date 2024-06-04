a3dc3c7ec185c39e48c90793143e1fb40d9a5528
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 08:30:41 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ const destinationTW = argv.destination || path.join(process.cwd(), 'src/componen
 const sourceAppTW = argv.source || path.join(__dirname, 'AppTW/App.jsx');
 const destinationAppTW = argv.destination || path.join(process.cwd(), 'src/');
 
-const sourcePR= argv.source || path.join(__dirname, 'AppTW/App.jsx');
-const destinationPR = argv.destination || path.join(process.cwd(), 'src/');
+const sourcePR = argv.source || path.join(__dirname, 'PrivateRoute/PrivateRoute.jsx');
+const destinationPR = argv.destination || path.join(process.cwd(), 'src/components/PrivateRoute');
 
 async function RunReactLoginSignIn() {
     try {
         await fs.copy(source, destination);
+        await fs.copy(sourceApp, destinationApp);
+        await fs.copy(sourcePR, destinationPR)
         console.log('The Login and SignUp templete Successfully Createed');
     } catch (err) {
         console.error('Error copying folder:', err);
