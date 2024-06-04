479bb9ba233d90d35d4400b949b6a31d42a22389
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 08:49:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const destinationAppTW = argv.destination || path.join(process.cwd(), 'src/');
 const sourcePR = argv.source || path.join(__dirname, 'PrivateRoute');
 const destinationPR = argv.destination || path.join(process.cwd(), 'src/components/PrivateRoute');
 
-const sourceDash = argv.source || path.join(__dirname, 'PrivateRoute');
-const destinationDash = argv.destination || path.join(process.cwd(), 'src/components/PrivateRoute');
+const sourceDash = argv.source || path.join(__dirname, 'Dashboard');
+const destinationDash = argv.destination || path.join(process.cwd(), 'src/components/Dashboard');
 
 const sourceBackendMysqlNoMVC = argv.source || path.join(__dirname, 'BackEndMysqlNoMVC/server');
 const destinationBackendMysqlNoMVC = argv.destination || path.join(process.cwd(), 'server/');
@@ -28,7 +28,8 @@ async function RunReactLoginSignIn() {
     try {
         await fs.copy(source, destination);
         await fs.copy(sourceApp, destinationApp);
-        await fs.copy(sourcePR, destinationPR)
+        await fs.copy(sourcePR, destinationPR);
+        await fs.copy(sourceDash, destinationDash);
         console.log('The Login and SignUp templete Successfully Createed');
     } catch (err) {
         console.error('Error copying folder:', err);
@@ -42,6 +43,7 @@ async function RunReactLoginSignInTW() {
         await fs.copy(sourceTW, destinationTW);
         await fs.copy(sourceAppTW, destinationAppTW);
         await fs.copy(sourcePR, destinationPR);
+        await fs.copy(sourceDash, destinationDash);
         console.log('The Login and SignUp templete Successfully Createed');
     } catch (err) {
         console.error('Error copying folder:', err);
