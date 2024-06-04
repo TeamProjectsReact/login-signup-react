06ce3a902f035f82e034f7e64f784902922e0656
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 09:06:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- LoginSignUpTW/SignUp.jsx
+++ LoginSignUpTW/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BsMortarboardFill } from "react-icons/bs";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 
 const SignUp = () => {
