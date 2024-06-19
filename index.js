28e81e05b4c5b27d48f9098656e7bbe8c3a2e753
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-19 08:03:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,81 +2,65 @@ const fs = require('fs-extra');
 const path = require('path');
 const argv = require('yargs').argv;
 
-const type = process.argv[2];
-const name = process.argv[3];
+const source = argv.source || path.join(__dirname, 'LoginSignUp');
+const destination = argv.destination || path.join(process.cwd(), 'src/components/LoginSignUp');
 
-if (!type || !name) {
-    console.error('Please provide Valied Command');
-    process.exit(1);
-}
-let fileContent;
-let filePath;
+const sourceApp = argv.source || path.join(__dirname, 'App');
+const destinationApp = argv.destination || path.join(process.cwd(), 'src/');
 
-if (type === 'loginSignUp') {
-    const source = argv.source || path.join(__dirname, 'LoginSignUp');
-    const destination = argv.destination || path.join(process.cwd(), 'src/components/LoginSignUp');
+const sourceTW = argv.source || path.join(__dirname, 'LoginSignUpTW');
+const destinationTW = argv.destination || path.join(process.cwd(), 'src/components/LoginSignUpTW');
 
-    async function RunReactLoginSignIn() {
-        try {
-            await fs.copy(source, destination);
-            console.log('The Login and SignUp templete Successfully Createed');
-        } catch (err) {
-            console.error('Error copying folder:', err);
-        }
+const sourceAppTW = argv.source || path.join(__dirname, 'AppTW');
+const destinationAppTW = argv.destination || path.join(process.cwd(), 'src/');
+
+const sourcePR = argv.source || path.join(__dirname, 'PrivateRoute');
+const destinationPR = argv.destination || path.join(process.cwd(), 'src/components/PrivateRoute');
+
+const sourceDash = argv.source || path.join(__dirname, 'Dashboard');
+const destinationDash = argv.destination || path.join(process.cwd(), 'src/components/Dashboard');
+
+const sourceBackendMysqlNoMVC = argv.source || path.join(__dirname, 'BackEndMysqlNoMVC');
+const destinationBackendMysqlNoMVC = argv.destination || path.join(process.cwd(), '../');
+
+
+async function RunReactLoginSignIn() {
+    try {
+        await fs.copy(source, destination);
+        await fs.copy(sourceApp, destinationApp);
+        await fs.copy(sourcePR, destinationPR);
+        await fs.copy(sourceDash, destinationDash);
+        console.log('The Login and SignUp templete Successfully Createed');
+    } catch (err) {
+        console.error('Error copying folder:', err);
     }
-
-    module.exports = RunReactLoginSignIn    
-} 
-
-// console.log(`${type.charAt(0).toUpperCase() + type.slice(1)} ${name} created at ${filePath}`);
-
-
-// const sourceApp = argv.source || path.join(__dirname, 'App');
-// const destinationApp = argv.destination || path.join(process.cwd(), 'src/');
-
-// const sourceTW = argv.source || path.join(__dirname, 'LoginSignUpTW');
-// const destinationTW = argv.destination || path.join(process.cwd(), 'src/components/LoginSignUpTW');
-
-// const sourceAppTW = argv.source || path.join(__dirname, 'AppTW');
-// const destinationAppTW = argv.destination || path.join(process.cwd(), 'src/');
-
-// const sourcePR = argv.source || path.join(__dirname, 'PrivateRoute');
-// const destinationPR = argv.destination || path.join(process.cwd(), 'src/components/PrivateRoute');
-
-// const sourceDash = argv.source || path.join(__dirname, 'Dashboard');
-// const destinationDash = argv.destination || path.join(process.cwd(), 'src/components/Dashboard');
-
-// const sourceBackendMysqlNoMVC = argv.source || path.join(__dirname, 'BackEndMysqlNoMVC');
-// const destinationBackendMysqlNoMVC = argv.destination || path.join(process.cwd(), '../');
+}
 
 
 
+async function RunReactLoginSignInTW() {
+    try {
+        await fs.copy(sourceTW, destinationTW);
+        await fs.copy(sourceAppTW, destinationAppTW);
+        await fs.copy(sourcePR, destinationPR);
+        await fs.copy(sourceDash, destinationDash);
+        console.log('The Login and SignUp templete Successfully Createed');
+    } catch (err) {
+        console.error('Error copying folder:', err);
+    }
+}
 
+async function RunBackendMysqlNoMVC() {
+    try {
+        await fs.copy(sourceBackendMysqlNoMVC, destinationBackendMysqlNoMVC);
+        console.log('Backend Development Enviroment Created Successful');
+    } catch (err) {
+        console.error('Error copying folder:', err);
+    }
+}
 
-
-// async function RunReactLoginSignInTW() {
-//     try {
-//         await fs.copy(sourceTW, destinationTW);
-//         await fs.copy(sourceAppTW, destinationAppTW);
-//         await fs.copy(sourcePR, destinationPR);
-//         await fs.copy(sourceDash, destinationDash);
-//         console.log('The Login and SignUp templete Successfully Createed');
-//     } catch (err) {
-//         console.error('Error copying folder:', err);
-//     }
-// }
-
-// async function RunBackendMysqlNoMVC() {
-//     try {
-//         await fs.copy(sourceBackendMysqlNoMVC, destinationBackendMysqlNoMVC);
-//         console.log('Backend Development Enviroment Created Successful');
-//     } catch (err) {
-//         console.error('Error copying folder:', err);
-//     }
-// }
-
-// module.exports = {
-//     RunReactLoginSignIn,
-//     RunReactLoginSignInTW,
-//     RunBackendMysqlNoMVC
-// }
+module.exports = {
+    RunReactLoginSignIn,
+    RunReactLoginSignInTW,
+    RunBackendMysqlNoMVC
+}
